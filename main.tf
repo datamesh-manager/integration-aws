dfb9eb83f595968ef345a3007cc7f9d4d1660bf7
Stefan Negele <stefan.negele@innoq.com>
2023-06-15 14:25:38 +0200

    Enable logging

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
-variable "process_feed_version" {
-  type = string
+variable "versions" {
+  type = object({
+    process_feed = string
+  })
 }
 
 variable "aws" {
@@ -9,6 +11,12 @@ variable "aws" {
     secret_key = string
   })
   sensitive = true
+}
+
+variable "dmm" {
+  type = object({
+    api_key = string
+  })
 }
 
 terraform {
@@ -39,17 +47,52 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
+resource "aws_iam_role" "process_feed_iam_role" {
   name               = "iam_for_lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
+resource "aws_iam_role_policy_attachment" "terraform_lambda_policy" {
+  role       = aws_iam_role.process_feed_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
 resource "aws_lambda_function" "process_feed_lambda_function" {
-  filename      = "${path.module}/out/process_feed_${var.process_feed_version}.zip"
+  # todo: move lambda build versions to s3
+  filename      = "${path.module}/out/process_feed_${var.versions.process_feed}.zip"
   function_name = "permissions__process_feed"
-  role          = aws_iam_role.iam_for_lambda.arn
+  role          = aws_iam_role.process_feed_iam_role.arn
   handler       = "process_feed.lambda_handler"
 
   runtime       = "python3.10"
   architectures = ["arm64"]
+}
+
+resource "aws_secretsmanager_secret" "process_feed_dmm_api_key" {
+  name                           = "process_feed_dmm_api_key"
+  force_overwrite_replica_secret = true # make sure to override secret
+  recovery_window_in_days        = 0 # force deletion on destroy
+}
+
+resource "aws_secretsmanager_secret_version" "dmm_api_key" {
+  secret_id     = aws_secretsmanager_secret.process_feed_dmm_api_key.id
+  secret_string = var.dmm.api_key
+}
+
+resource "aws_iam_role_policy" "sm_policy" {
+  name = "process_feed_dmm_api_key_access"
+  role = aws_iam_role.process_feed_iam_role.id
+
+  policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "secretsmanager:GetSecretValue",
+        ]
+        Effect   = "Allow"
+        Resource = aws_secretsmanager_secret.process_feed_dmm_api_key.arn
+      }
+    ]
+  })
 }
--- src/process_feed/process_feed.py
+++ src/process_feed/process_feed.py
@@ -1,5 +1,35 @@
 import requests
+from boto3.session import Session
+from botocore.exceptions import ClientError
 
 
 def lambda_handler(event, context):
-    return "process"
+    print(event)
+
+
+    api_key()
+    return
+
+
+def api_key() -> str:
+    secret_name = "process_feed_dmm_api_key"
+    region_name = "eu-central-1"
+
+    # Create a Secrets Manager client
+    session = Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    # Decrypts secret using the associated KMS key.
+    return get_secret_value_response['SecretString']
