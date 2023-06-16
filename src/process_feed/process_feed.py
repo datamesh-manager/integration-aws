4dc8cd0bf566df75aed87c0b4cc4a54193ff4b5f
Stefan Negele <stefan.negele@innoq.com>
2023-06-16 16:12:17 +0200

    Iterate through events

--- src/process_feed/process_feed.py
+++ src/process_feed/process_feed.py
@@ -1,22 +1,31 @@
-from requests import get
+from requests import get, Response
 from boto3.session import Session
 
 
 def lambda_handler(event, context):
-    key = api_key("process_feed_dmm_api_key", "eu-central-1")
-    r = get_events(key)
+    key = api_key('process_feed_dmm_api_key', 'eu-central-1')
 
-    print(r.json())
+    last_event_id = None
+
+    while True:
+        response_elements = get_events(key, last_event_id).json()
+
+        if len(response_elements) == 0:
+            break
+        else:
+            for element in response_elements:
+                print(element)
+                last_event_id = element['id']
 
     return
 
 
-def get_events(key: str, last_event_id: str):
+def get_events(key: str, last_event_id: str) -> Response:
     response = get(
         url=events_url(last_event_id),
         headers={
-            "x-api-key": key,
-            "accept": "application/cloudevents-batch+json"
+            'x-api-key': key,
+            'accept': 'application/cloudevents-batch+json'
         })
     response.raise_for_status()
 
@@ -24,10 +33,9 @@ def get_events(key: str, last_event_id: str):
 
 
 def events_url(last_event_id: str) -> str:
-    base_url = "https://app.datamesh-manager.com/api/events"
-
-    base_url if last_event_id is None \
-        else "{url}?lastEventId={id}".format(url=base_url, id=last_event_id)
+    base_url = 'https://app.datamesh-manager.com/api/events'
+    return base_url if last_event_id is None \
+        else '{url}?lastEventId={id}'.format(url=base_url, id=last_event_id)
 
 
 def api_key(secret_name: str, region_name: str) -> str:
