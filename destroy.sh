4dd2b09787816bc73c966518be32ce8b132da625
Stefan Negele <stefan.negele@innoq.com>
2023-06-20 16:24:57 +0200

    Reformat

--- destroy.sh
+++ destroy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 aws s3 rb --force "s3://dmm-integration"
-terraform -chdir=terraform destroy -var='versions={"process_feed":"0","process_events":"0"}' -auto-approve
+terraform -chdir=terraform destroy\
+  -var='versions={"process_feed":"0","process_events":"0"}'\
+  -auto-approve
