c7bb5cc4cb683a1b43d3860bcfa13011a3a43dff
Stefan Negele <stefan.negele@innoq.com>
2023-06-30 13:40:02 +0200

    Add DMM calls to process_events

--- src/process_events/lambda_handler.py
+++ src/process_events/lambda_handler.py
@@ -1,15 +1,22 @@
 import json
 import logging
+from typing import TypeAlias
 
 import boto3
-from requests import get
+import requests
+
+DataContract: TypeAlias = dict[str, dict[str, str]]
+Port: TypeAlias = dict[str, dict[str, str]]
+DataProduct: TypeAlias = dict[str, dict[str, str] | list[Port]]
 
 
 def lambda_handler(event, context):
     logging.getLogger().setLevel(logging.INFO)
 
     dmm_events = list(map(lambda e: json.loads(e['body']), event['Records']))
-    api_key = dmm_api_key('dmm_integration__api_key')
+
+    secrets = Secrets(boto3.client('secretsmanager'))
+    api_key = secrets.get_secret('dmm_integration__api_key')
 
     for dmm_event in dmm_events:
         process_event(api_key, dmm_event)
@@ -28,29 +35,59 @@ def process_event(api_key: str, event: dict[str, str | dict]):
 
 
 def process_activated_event(api_key: str, event: dict[str, str | dict]):
-    # datacontract_id = event['data']['id']
+    datacontract_id = event['data']['id']
     # get_datacontract(api_key, datacontract_id)
+    client = DMMClient('https://app.datamesh-manager.com/api/events',
+                       api_key)
+    client.get_datacontract(datacontract_id)
 
     logging.info('Activated: {}'.format(event['id']))
 
 
-def get_datacontract(
-    api_key: str,
-    datacontract_id: str
-) -> dict[str, dict[str, str]]:
-    response = get(
-        url='/api/datacontracts/{}'.format(datacontract_id),
-        headers={
-            'x-api-key': api_key,
-            'accept': 'application/cloudevents-batch+json'
-        })
-    response.raise_for_status()
-
-    return response.json()
-
-
 def process_deactivated_event(event):
     logging.info('Deactivated: {}'.format(event['id']))
+
+
+class DMMClient:
+    def __init__(self, base_url: str, api_key: str):
+        self._base_url = base_url
+        self._api_key = api_key
+
+    def get_datacontract(self, datacontract_id: str) -> DataContract | None:
+        response = self._get(self._get_datacontract_url(datacontract_id))
+
+        if response.status_code == 404:
+            logging.warning(
+                'No datacontract with id {}'.format(datacontract_id))
+            return None
+        else:
+            response.raise_for_status()
+            return response.json()
+
+    def _get_datacontract_url(self, datacontract_id) -> str:
+        return '{base_url}/api/datacontracts/{id}'.format(
+            base_url=self._base_url, id=datacontract_id)
+
+    def get_dataproduct(self, dataproduct_id) -> DataProduct | None:
+        response = self._get(self._get_dataproduct_url(dataproduct_id))
+
+        if response.status_code == 404:
+            logging.warning(
+                'No dataproduct with id {}'.format(dataproduct_id))
+            return None
+        else:
+            response.raise_for_status()
+            return response.json()
+
+    def _get_dataproduct_url(self, dataproduct_id) -> str:
+        return '{base_url}/api/dataproducts/{id}'.format(
+            base_url=self._base_url, id=dataproduct_id)
+
+    def _get(self, url):
+        return requests.get(
+            url=url,
+            headers={'x-api-key': self._api_key,
+                     'accept': 'application/json'})
 
 
 class Secrets:
--- src/process_events/test_lambda_handler.py
+++ src/process_events/test_lambda_handler.py
@@ -1,14 +1,128 @@
 import unittest
 from unittest import TestCase
+from unittest.mock import patch, sentinel, Mock
 
 import boto3
 from botocore.stub import Stubber
 
-from lambda_handler import Secrets
+from lambda_handler import Secrets, DMMClient
+
+
+class TestDMMClient(TestCase):
+    _base_url = 'https://dmm-url.com'
+    _api_key = 'supersecret'
+    _datacontract_id = '123'
+    _dataproduct_id = '987'
+
+    def setUp(self) -> None:
+        self._client = DMMClient(self._base_url, self._api_key)
+
+    class MockResponse:
+        def __init__(self, body, status):
+            self._body = body
+            self.status_code = status
+
+        def json(self) -> str:
+            return self._body
+
+        def raise_for_status(self) -> None:
+            if self.status_code >= 400:
+                raise Exception()
+
+    # commonly used
+
+    @staticmethod
+    def mock_get__api_key(**kwargs) -> MockResponse:
+        if kwargs['headers']['x-api-key'] == TestDMMClient._api_key:
+            return TestDMMClient.MockResponse(sentinel.expected, 200)
+        else:
+            return TestDMMClient.MockResponse(sentinel.something, 500)
+
+    # get_datacontract
+
+    @staticmethod
+    def mock_get_datacontract(**kwargs) -> MockResponse:
+        expected_url = '{base_url}/api/datacontracts/{id}'.format(
+            base_url=TestDMMClient._base_url,
+            id=TestDMMClient._datacontract_id)
+
+        if kwargs['url'] == expected_url:
+            return TestDMMClient.MockResponse(sentinel.expected, 200)
+        else:
+            return TestDMMClient.MockResponse(None, 200)
+
+    @patch('requests.get', Mock(side_effect=mock_get_datacontract))
+    def test_get_datacontract(self) -> None:
+        self.assertEqual(sentinel.expected,
+                         self._client.get_datacontract(self._datacontract_id))
+
+    @staticmethod
+    def mock_get_datacontract_not_found(**kwargs) -> MockResponse:
+        return TestDMMClient.MockResponse(sentinel.something, 404)
+
+    @patch('requests.get', Mock(side_effect=mock_get_datacontract_not_found))
+    def test_get_datacontract_not_found(self) -> None:
+        self.assertEqual(None,
+                         self._client.get_datacontract(self._datacontract_id))
+
+    @staticmethod
+    def mock_get_datacontract_other_error() -> MockResponse:
+        return TestDMMClient.MockResponse(sentinel.something, 500)
+
+    @patch('requests.get', Mock(side_effect=mock_get_datacontract_other_error))
+    def test_get_datacontract_other_error(self) -> None:
+        with self.assertRaises(Exception):
+            self._client.get_datacontract(self._datacontract_id)
+
+    @patch('requests.get', Mock(side_effect=mock_get__api_key))
+    def test_get_datacontract_api_key(self) -> None:
+        self.assertEqual(sentinel.expected,
+                         self._client.get_datacontract(self._datacontract_id))
+
+    # get_dataproduct
+
+    @staticmethod
+    def mock_get_dataproduct(**kwargs) -> MockResponse:
+        expected_url = '{base_url}/api/dataproducts/{id}'.format(
+            base_url=TestDMMClient._base_url,
+            id=TestDMMClient._dataproduct_id)
+
+        if kwargs['url'] == expected_url:
+            return TestDMMClient.MockResponse(sentinel.expected, 200)
+        else:
+            return TestDMMClient.MockResponse(None, 200)
+
+    @patch('requests.get', Mock(side_effect=mock_get_dataproduct))
+    def test_get_dataproduct(self) -> None:
+        self.assertEqual(sentinel.expected,
+                         self._client.get_dataproduct(self._dataproduct_id))
+
+    @staticmethod
+    def mock_get_dataproduct_not_found(**kwargs) -> MockResponse:
+        return TestDMMClient.MockResponse(sentinel.something, 404)
+
+    @patch('requests.get', Mock(side_effect=mock_get_dataproduct_not_found))
+    def test_get_dataproduct_not_found(self) -> None:
+        self.assertEqual(None,
+                         self._client.get_dataproduct(self._dataproduct_id))
+
+    @staticmethod
+    def mock_get_dataproduct_other_error(**kwargs) -> MockResponse:
+        return TestDMMClient.MockResponse(sentinel.something, 500)
+
+    @patch('requests.get', Mock(side_effect=mock_get_dataproduct_other_error))
+    def test_get_dataproduct_other_error(self) -> None:
+        with self.assertRaises(Exception):
+            self._client.get_dataproduct(self._dataproduct_id)
+
+    @patch('requests.get', Mock(side_effect=mock_get__api_key))
+    def test_get_dataproduct_api_key(self) -> None:
+        self.assertEqual(sentinel.expected,
+                         self._client.get_dataproduct(self._dataproduct_id))
 
 
 class TestSecrets(TestCase):
-    _secret_name = 'a_name'
+    _secret_name = 'configured_name'
     _secret_value = 'hi!_i_am_secret'
 
     def setUp(self) -> None:
