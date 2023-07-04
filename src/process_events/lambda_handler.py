55efaf12c425f8f60b0e0bc5ac87dafaf01f3b3f
Stefan Negele <stefan.negele@innoq.com>
2023-07-04 18:03:38 +0200

    Add event handler implementation

--- src/process_events/lambda_handler.py
+++ src/process_events/lambda_handler.py
@@ -3,53 +3,29 @@ import logging
 from datetime import datetime
 from typing import TypeAlias
 
-import boto3
 import requests
 
 DataContract: TypeAlias = dict[str, dict[str, str]]
 Port: TypeAlias = dict[str, dict[str, str]]
 DataProduct: TypeAlias = dict[str, dict[str, str] | list[Port]]
+DMMEvent: TypeAlias = dict[str, str | dict]
 
 
 def lambda_handler(event, context):
     logging.getLogger().setLevel(logging.INFO)
 
-    resource_explorer = boto3.client('resource-explorer-2')
-    iam = boto3.client('iam')
-
-    dmm_events = list(map(lambda e: json.loads(e['body']), event['Records']))
-
-    secrets = Secrets(boto3.client('secretsmanager'))
-    api_key = secrets.get_secret('dmm_integration__api_key')
-
-    for dmm_event in dmm_events:
-        process_event(api_key, dmm_event)
+    # resource_explorer = boto3.client('resource-explorer-2')
+    # iam = boto3.client('iam')
+    #
+    # dmm_events = list(map(lambda e: json.loads(e['body']), event['Records']))
+    #
+    # secrets = Secrets(boto3.client('secretsmanager'))
+    # api_key = secrets.get_secret('dmm_integration__api_key')
+    #
+    # for dmm_event in dmm_events:
+    #     process_event(api_key, dmm_event)
 
     return
-
-
-def process_event(api_key: str, event: dict[str, str | dict]):
-    logging.info('Process event: {}'.format(event))
-
-    match event['type']:
-        case 'com.datamesh-manager.events.DataContractActivatedEvent':
-            process_activated_event(api_key, event)
-        case 'com.datamesh-manager.events.DataContractDeactivatedEvent':
-            process_deactivated_event(event)
-
-
-def process_activated_event(api_key: str, event: dict[str, str | dict]):
-    datacontract_id = event['data']['id']
-    # get_datacontract(api_key, datacontract_id)
-    client = DMMClient('https://app.datamesh-manager.com/api/events',
-                       api_key)
-    client.get_datacontract(datacontract_id)
-
-    logging.info('Activated: {}'.format(event['id']))
-
-
-def process_deactivated_event(event):
-    logging.info('Deactivated: {}'.format(event['id']))
 
 
 class DMMClient:
@@ -174,7 +150,8 @@ class AccessManager:
         create_policy_result = self._iam.create_policy(
             PolicyName='DMM Datacontract {}'.format(datacontract_id),
             PolicyDocument=json.dumps(policy_document),
-            Tags=[self.managed_by_tag(), self._contract_id_tag(datacontract_id)]
+            Tags=[self._managed_by_tag(),
+                  self._contract_id_tag(datacontract_id)]
         )
 
         # attach it to the consumer iam role
@@ -213,7 +190,7 @@ class AccessManager:
         }
 
     @staticmethod
-    def managed_by_tag() -> dict[str, str]:
+    def _managed_by_tag() -> dict[str, str]:
         return {
             'Key': 'managed-by',
             'Value': 'dmm-integration'
@@ -234,3 +211,72 @@ class AccessManager:
 class UnsupportedServiceException(Exception):
     def __init__(self, service_name):
         super().__init__("Unsupported service: {}".format(service_name))
+
+
+class EventHandler:
+    def __init__(self, dmm_client: DMMClient, access_manager: AccessManager):
+        self._dmm_client = dmm_client
+        self._access_manager = access_manager
+
+    def handle(self, event: DMMEvent) -> None:
+        logging.info('Process event: {}'.format(event))
+        match event['type']:
+            case 'com.datamesh-manager.events.DataContractDeactivatedEvent':
+                self._deactivated_event(event)
+            case 'com.datamesh-manager.events.DataContractActivatedEvent':
+                self._activated_event(event)
+
+    def _deactivated_event(self, event: DMMEvent):
+        self._access_manager.remove_access(event['data']['id'])
+
+    def _activated_event(self, event: DMMEvent):
+        datacontract_id = event['data']['id']
+
+        datacontract = self._dmm_client.get_datacontract(datacontract_id)
+
+        consumer_role_name = self._consumer_role_name(
+            datacontract['consumer']['dataProductId'])
+
+        output_port_arn = self._output_port_arn(
+            datacontract['provider']['dataProductId'],
+            datacontract['provider']['outputPortId'])
+
+        self._access_manager.grant_access(datacontract_id,
+                                          consumer_role_name,
+                                          output_port_arn)
+
+        logging.info('Activated: {}'.format(event['id']))
+
+    def _consumer_role_name(self, consumer_dataproduct_id):
+        consumer_dataproduct = self._dmm_client.get_dataproduct(
+            consumer_dataproduct_id)
+
+        try:
+            consumer_role_name = consumer_dataproduct['custom']['aws-role-name']
+        except KeyError as ke:
+            raise RequiredCustomFieldNotSet(ke)
+
+        return consumer_role_name
+
+    def _output_port_arn(self,
+        provider_dataproduct_id,
+        provider_output_port_id) -> str:
+
+        provider_dataproduct = self._dmm_client.get_dataproduct(
+            provider_dataproduct_id)
+
+        output_port = next(op
+                           for op in provider_dataproduct['outputPorts']
+                           if op['id'] == provider_output_port_id)
+
+        try:
+            output_port_arn = output_port['custom']['aws-arn']
+        except KeyError as ke:
+            raise RequiredCustomFieldNotSet(ke)
+
+        return output_port_arn
+
+
+class RequiredCustomFieldNotSet(Exception):
+    def __init__(self, field_name):
+        super().__init__("Custom field must be set: {}".format(field_name))
--- src/process_events/test_lambda_handler.py
+++ src/process_events/test_lambda_handler.py
@@ -8,8 +8,8 @@ from unittest.mock import patch, sentinel, Mock
 import boto3
 from botocore.stub import Stubber
 
-from lambda_handler import Secrets, DMMClient, AccessManager, \
-    UnsupportedServiceException
+from lambda_handler import Secrets, DMMClient, AccessManager, EventHandler, \
+    UnsupportedServiceException, RequiredCustomFieldNotSet
 
 
 class TestDMMClient(TestCase):
@@ -376,6 +376,152 @@ class TestAccessManager(TestCase):
             {},
             expected_params
         )
+
+
+class TestEventHandler(TestCase):
+    _event_id = '123-123-123-123'
+    _data_contract_id = '999-888-777'
+    _consumer_role_name = 'consumer_role_123'
+    _consumer_dataproduct_id = 'asdf-123-asdf'
+    _output_port_id = '456-6454-545'
+    _output_port_arn = 'arn:aws:service:output:port'
+    _provider_dataproduct_id = 'qwer-321-qwer'
+
+    _activated_event = {
+        'id': _event_id,
+        'type': 'com.datamesh-manager.events.DataContractActivatedEvent',
+        'data': {'id': _data_contract_id}
+    }
+
+    @patch('lambda_handler.AccessManager')
+    @patch('lambda_handler.DMMClient')
+    def setUp(self, dmm_client, access_manager) -> None:
+        self._dmm_client = dmm_client
+        self._access_manager = access_manager
+        self._event_handler = EventHandler(dmm_client, access_manager)
+
+    def tearDown(self) -> None:
+        self._dmm_client.reset_mock()
+        self._access_manager.reset_mock()
+
+    def test_handle__ignore_others(self) -> None:
+        event = {
+            'id': self._event_id,
+            'type': 'com.datamesh-manager.events.OtherEvent'
+        }
+        self._event_handler.handle(event)
+        self._access_manager.grant_access.assert_not_called()
+        self._access_manager.remove_access.assert_not_called()
+
+    def test_handle__deactivated(self) -> None:
+        event = {
+            'id': self._event_id,
+            'type': 'com.datamesh-manager.events.DataContractDeactivatedEvent',
+            'data': {'id': self._data_contract_id}
+        }
+
+        self._event_handler.handle(event)
+
+        self._access_manager.remove_access.assert_called_with(
+            self._data_contract_id)
+
+    def test_handle__activated(self) -> None:
+        self._dmm_client.get_datacontract = self._mock_get_data_contract
+        self._dmm_client.get_dataproduct = self._mock_get_dataproduct
+
+        self._event_handler.handle(self._activated_event)
+
+        self._access_manager.grant_access.assert_called_with(
+            self._data_contract_id,
+            self._consumer_role_name,
+            self._output_port_arn)
+
+    def test_handle__activated__consumer_role_not_set(self) -> None:
+        self._dmm_client.get_datacontract = self._mock_get_data_contract
+        self._dmm_client.get_dataproduct = self._mock_get_dataproduct_no_role
+
+        with self.assertRaises(RequiredCustomFieldNotSet):
+            self._event_handler.handle(self._activated_event)
+
+    def test_handle__activated__provider_arn_not_set(self) -> None:
+        self._dmm_client.get_datacontract = self._mock_get_data_contract
+        self._dmm_client.get_dataproduct = self._mock_get_dataproduct_no_arn
+
+        with self.assertRaises(RequiredCustomFieldNotSet):
+            self._event_handler.handle(self._activated_event)
+
+    def _mock_get_data_contract(self, datacontract_id: str):
+        if datacontract_id == self._data_contract_id:
+            return {
+                'consumer': {
+                    'dataProductId': self._consumer_dataproduct_id
+                },
+                'provider': {
+                    'dataProductId': self._provider_dataproduct_id,
+                    'outputPortId': self._output_port_id
+                }
+            }
+        else:
+            return None
+
+    def _mock_get_dataproduct(self, dataproduct_id: str):
+        if dataproduct_id == self._consumer_dataproduct_id:
+            return {
+                'custom': {
+                    'aws-role-name': self._consumer_role_name
+                }
+            }
+        elif dataproduct_id == self._provider_dataproduct_id:
+            return {
+                'outputPorts': [
+                    {
+                        'id': self._output_port_id,
+                        'custom': {
+                            'aws-arn': self._output_port_arn
+                        }
+                    }
+                ]
+            }
+        else:
+            return None
+
+    def _mock_get_dataproduct_no_role(self, dataproduct_id: str):
+        if dataproduct_id == self._consumer_dataproduct_id:
+            return {
+                'custom': {}
+            }
+        elif dataproduct_id == self._provider_dataproduct_id:
+            return {
+                'outputPorts': [
+                    {
+                        'id': self._output_port_id,
+                        'custom': {
+                            'aws-arn': self._output_port_arn
+                        }
+                    }
+                ]
+            }
+        else:
+            return None
+
+    def _mock_get_dataproduct_no_arn(self, dataproduct_id: str):
+        if dataproduct_id == self._consumer_dataproduct_id:
+            return {
+                'custom': {
+                    'aws-role-name': self._consumer_role_name
+                }
+            }
+        elif dataproduct_id == self._provider_dataproduct_id:
+            return {
+                'outputPorts': [
+                    {
+                        'id': self._output_port_id,
+                        'custom': {}
+                    }
+                ]
+            }
+        else:
+            return None
 
 
 if __name__ == '__main__':
